cae9f5f3f8143c98cd2869e6f4a7bec871cbcb27
vaibhav <chaudharivaibhav60@gmail.com>
2021-10-31 13:43:53 +0530

    3rd assignment done

---
+++ OneDrive/Desktop/react-3rd-assignment/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Textform from "./components/textform";
+function App() {
+  return (
+    <div className="App">
+      <Textform />
+    </div>
+  );
+}
+
+export default App;
